469d894858bbe4750f36c976068c25378bf8357a
Skilled <skilled5041@gmail.com>
2023-05-22 12:49:06 -0400

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "carbon-components-svelte/css/g100.css";
+    import { Header, HeaderNav, HeaderNavItem } from "carbon-components-svelte";
+</script>
+
+<Header company="Solarion2" platformName="Kit-Bot">
+    <HeaderNav>
+        <HeaderNavItem href="/">Home</HeaderNavItem>
+        <HeaderNavItem href="/about">About</HeaderNavItem>
+        <HeaderNavItem href="/contact">Contact</HeaderNavItem>
+    </HeaderNav>
+</Header>
+
+<slot/>
